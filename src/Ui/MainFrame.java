82cd330e3f7d12e0a68d6e998b9623ea18ad6387
977903096 <977903096@qq.com>
2014-10-17 19:43:31 +0800

    2014/10/17 将棋子放置到棋盘上，但不能实现棋子的移动

--- src/Ui/MainFrame.java
+++ src/Ui/MainFrame.java
@@ -3,18 +3,24 @@ package Ui;
 import java.awt.Color;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.Border;
-
+/*
+ * 定义MainFrame继承Jfame类。
+ * 在该类中定义组件，实现界面
+ * 
+ * */
 public class MainFrame extends JFrame{
 	public static void main(String[] args) {
 		new MainFrame().init();
 	}
-	//定义组件
+	//定义棋盘组件
 	JPanel panel;
 	JPanel panel2;
 	JTextArea area;
@@ -22,8 +28,14 @@ public class MainFrame extends JFrame{
 	JButton btn2;
 	JScrollPane js;
 	Border b = BorderFactory.createLineBorder(Color.red);
+	//定义棋子组件
+	JLabel red ;
+	JLabel redking = new JLabel(new ImageIcon("img\\redking.png"));
+	JLabel blue;
+	JLabel blueking = new JLabel(new ImageIcon("img\\blueking.png"));
 	private void init() {
 		//初始化组件
+		
 		this.setLayout(null);
 		panel = new JPanel();
 		panel.setLayout(null);
@@ -39,9 +51,55 @@ public class MainFrame extends JFrame{
 		btn2.setBounds(120, 315, 60,40);
 		this.add(panel);
 		panel.setBounds(601, 0, 199,600);
-		
+		//绘制棋盘，并将棋子放置到棋盘上
        panel2 = new Mypanel();
 		panel2.setBorder(b);
+		panel2.setLayout(null);
+		//添加红子
+		int h = 0;
+		int w = 0;
+		for(int i = 1; i <= 4;i++ ){
+			if(i%2==1){
+				w = 60;
+				for(int j = 1;j <= 5;j++){
+				red =  new JLabel( new ImageIcon("img\\red.png"));
+				panel2.add(red);
+				red.setBounds(w, h, 60,60);
+				w+=120;
+				}
+			}else{
+				w = 0;
+				for(int j = 1;j <= 5;j++){
+				red =  new JLabel( new ImageIcon("img\\red.png"));
+				panel2.add(red);
+				red.setBounds(w, h, 60, 60);
+				w+=120;
+				}
+			}
+			h+=60;
+		}
+		//添加篮子
+		h = 360;
+		for(int i = 1; i <= 4;i++ ){
+			if(i%2==1){
+				w = 60;
+				for(int j = 1;j <= 5;j++){
+				blue = new JLabel(new ImageIcon("img\\blue.png"));
+				panel2.add(blue);
+				blue.setBounds(w, h, 60,60);
+				w+=120;
+				}
+			}else{
+				w = 0;
+				for(int j = 1;j <= 5;j++){
+			   blue = new JLabel(new ImageIcon("img\\blue.png"));
+				panel2.add(blue);
+				blue.setBounds(w, h, 60, 60);
+				w+=120;
+				}
+			}
+			h+=60;
+		}
 		this.getContentPane().add(panel2);
 		panel2.setBounds(0, 0,600,600);
 		this.setBounds(200, 70, 800,630);
--- src/Ui/Mypanel.java
+++ src/Ui/Mypanel.java
@@ -5,7 +5,10 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
-
+/*
+ * 定义Mypanel继承Jpanel类。复写paintComponent方法
+ * 通过复写方法，绘制出棋盘。
+ * */
 public class Mypanel extends JPanel{
 	Graphics2D  g2;
 	protected void paintComponent(Graphics g) {
@@ -16,10 +19,10 @@ public class Mypanel extends JPanel{
 			  if(i%2==1){
 			    for(int j=1;j<=10;j++){
 			          if(j%2==0){
-			        	  g.setColor(Color.red);
+			        	  g.setColor(Color.gray);
 			        	  g.fillRect(w, h, 60, 60);
 			          }else{
-			        	  g.setColor(Color.blue);
+			        	  g.setColor(Color.green);
 			        	  g.fillRect(w, h, 60, 60);
 			          }
 			          w+=60;
@@ -29,10 +32,10 @@ public class Mypanel extends JPanel{
 				    for(int j=1;j<=10;j++){
 				          
 				          if(j%2==1){
-				        	  g.setColor(Color.red);
+				        	  g.setColor(Color.gray);
 				        	  g.fillRect(w, h, 60, 60);
 				          }else{
-				        	  g.setColor(Color.blue);
+				        	  g.setColor(Color.green);
 				        	  g.fillRect(w, h, 60, 60);
 				          }
 				          w+=60;
